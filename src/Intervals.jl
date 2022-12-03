f9172a321ebc7651206951d0b185a21c812a291f
Roy Tseng <stu121645162002@gmail.com>
2022-12-04 01:14:05 +0800

    update

--- src/Intervals.jl
+++ src/Intervals.jl
@@ -3,7 +3,7 @@ module Intervals
 export OrderBlock, 
        Time,
        create_dummy, 
-       init_interval_list
+       init_interval_list!
     include("./structs.jl")
     include("./functions.jl")
 end # module
